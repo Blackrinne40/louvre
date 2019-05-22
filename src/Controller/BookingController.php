ac1ca4b024682b4df67ffb1ba4e79001e255ed01
Blackrinne <marine-221992@hotmail.fr>
2019-05-22 17:12:10 +0200

    Creation of HomeController and BookingController, creation of HomeView and Booking/index view

---
+++ src/Controller/BookingController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookingController extends AbstractController
+{
+    /**
+     * @Route("/booking", name="booking")
+     * @return Response
+     */
+    public function index():Response
+    {
+        return $this->render('booking/index.html.twig', [
+            'current_menu'=>'booking'
+        ]);
+    }
+}
