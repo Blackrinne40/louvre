deaae3923e7d293868a5e89bb219b77051e8aa2f
Blackrinne <marine-221992@hotmail.fr>
2019-06-04 13:14:25 +0200

    Add calculator + redirection to order

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\Booking;
-use App\Entity\Ticket;
 use App\Form\BookingTicketsType;
 use App\Services\PriceCalculator;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,22 +15,24 @@ class TicketController extends AbstractController
 {
     /**
      * @Route("/ticket", name="ticket")
+     * @param Request $request
      * @param SessionInterface $session
+     * @param PriceCalculator $calculator
      * @return Response
+     * @throws Exception
      */
-    public function index(SessionInterface $session, PriceCalculator $calculator):Response
+    public function index(Request $request,SessionInterface $session, PriceCalculator $calculator):Response
     {
 
         $booking = $session->get('booking');
 
-
         $form = $this->createForm(BookingTicketsType::class, $booking);
+        $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            // TODO 2 calculer le prix des billets
             $calculator->computeBookingPrice($booking);
+            $session->set('booking', $booking);
+            return $this->redirectToRoute('order');
 
-            // TODO 3 rediriger vers la page de recap
-            return $this->redirectToRoute('ticket');
         }
         return $this->render("ticket/index.html.twig", [
             'current_menu'=>'ticket',
